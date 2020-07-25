81816855aec5f6657f156c6d010c634a2aeaba32
joshyipkato <joshyipkato@gmail.com>
2020-07-25 16:36:46 +0800

    Update web.rb

--- web.rb
+++ web.rb
@@ -32,7 +32,7 @@ post '/ephemeral_keys' do
   
   begin
     key = Stripe::EphemeralKey.create(
-      {customer: @customer.id},
+      {customer: params["customer_id"]},
       {stripe_version: params["api_version"]}
     )
   rescue Stripe::StripeError => e
